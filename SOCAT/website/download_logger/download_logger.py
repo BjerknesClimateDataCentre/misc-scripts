14ff7530cf5271e5ac27c1086a21b48eae21fff3
Camilla <camilla.landa@uib.no>
2022-01-24 15:19:00 +0100

    (#1) Write script outline

---
+++ SOCAT/website/download_logger/download_logger.py
@@ -0,0 +1,23 @@
+# Import the stored info json file
+# !!!
+
+# Download the newest log files from socat.info:
+# - Connect to the server and find the log file location
+# - Download the current and previous log file into the temp folder
+# ...
+
+# Identify where in the files to start logging:
+# Open the previous log file and look for the previous log end date (found in
+# json file). If cannot find it, open and look through the current log file.
+# ...
+
+# FOR NOW: Import the test log file
+# !!!
+
+# Read line by line and identify downloads
+# - if find word 'zip', extract the IP adress and the filename.
+# - share this info with the Carbon Portal
+# ...
+
+# Update the previous log end date:
+# ...
